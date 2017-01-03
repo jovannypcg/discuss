d03eec02a8aa5a1f0e56a1466059e7ee10ca107b
Jovanny Cruz <jovannypcg@yahoo.com>
2017-01-03 12:41:18 -0600

    Add redirect instruction after creating a new topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -20,7 +20,10 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic created successfully")
+        |> redirect(to: "/")
       {:error, err_changeset} ->
         render conn, "new.html", changeset: err_changeset
     end
